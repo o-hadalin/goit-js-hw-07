4aa1f577b5ac1a3e2363591e75cc95241213bf83
O. HADALIN <dyblikat777@gmail.com>
2024-10-04 13:26:49 +0300

    Task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
+    const emailIn = form.elements.email.value.trim();
+    const passIn = form.elements.password.value.trim();
+    // console.log(emailIn, passIn);
+    
+
+    if (emailIn === "" || passIn === "") {
+        alert("All form fields must be filled in");
+        return;
+    };
+    
+    const formOut = {
+        email: emailIn,
+        password: passIn,
+    };
+    console.log(formOut);
+    form.reset();
+});
